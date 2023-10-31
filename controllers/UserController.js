a9dbaa7a8146cfa8d993303589533c34cf5c2c27
yanshin <yan.shin@cdsg.com.mm>
2023-10-31 16:38:21 +0630

    Register functionality

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,10 +3,14 @@ const bcrypt = require("bcrypt");
 
 const registerUser = async (req, res) => {
   const { username, password } = req.body;
-  // const existingUser = await User.findOne({ username });
-  const user = await User.create({ username: username, password: password });
-  res.send(user);
-  // const user = await User.create()
+  const existingUser = await User.findOne({ username });
+  if (existingUser != null) {
+    res.send("User exists");
+  } else {
+    const hashPass = await bcrypt.hash(password, 12);
+    const user = await User.create({ username: username, password: hashPass });
+    res.status(200).json({ user });
+  }
 };
 
 module.exports = { registerUser };
